b75a16dbd8636643e66921c199d34ed7d441ca0c
TatsuyaNakamori <***>
2021-02-07 01:17:05 +0900

    Add i18n files

---
+++ src/i18n.ts
@@ -0,0 +1,25 @@
+import * as vscode from 'vscode'
+
+const i18nFiles: { [key: string]: string; } = {
+    "en": "./../package.nls.json",
+    "ja": "./../package.nls.ja.json"
+}
+const i18nData = getData();
+
+
+export function localize(i18nKey: string) {
+    return i18nData[i18nKey];
+}
+
+function getData() {
+    const locale = vscode.env.language;
+    console.log("vscode.env.language= " + locale);
+
+    let i18nJSON = require(i18nFiles[locale]);
+    console.log("i18nJSON is:");
+    console.log(i18nJSON);
+
+    return i18nJSON
+}
+
+
