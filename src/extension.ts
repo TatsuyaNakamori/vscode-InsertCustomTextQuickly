85f7511da21cce4157169e6f6addc68c24f90908
TatsuyaNakamori <***>
2021-02-07 01:16:45 +0900

    set RegistCommand

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { InsertCustomText } from './InsertCustomText';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
+	console.log('"insertcustomtext" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "insertcustomtext" is now active!');
+	// registerCommand: KeyAction
+	let keyAction01 = new InsertCustomText(context, "insertcustomtext.keyAction01", "keyAction01.title", "keyAction.01")
+	let keyAction02 = new InsertCustomText(context, "insertcustomtext.keyAction02", "keyAction02.title", "keyAction.02")
+	let keyAction03 = new InsertCustomText(context, "insertcustomtext.keyAction03", "keyAction03.title", "keyAction.03")
+	let keyAction04 = new InsertCustomText(context, "insertcustomtext.keyAction04", "keyAction04.title", "keyAction.04")
+	let keyAction05 = new InsertCustomText(context, "insertcustomtext.keyAction05", "keyAction05.title", "keyAction.05")
+	let keyAction06 = new InsertCustomText(context, "insertcustomtext.keyAction06", "keyAction06.title", "keyAction.06")
+	let keyAction07 = new InsertCustomText(context, "insertcustomtext.keyAction07", "keyAction07.title", "keyAction.07")
+	let keyAction08 = new InsertCustomText(context, "insertcustomtext.keyAction08", "keyAction08.title", "keyAction.08")
+	let keyAction09 = new InsertCustomText(context, "insertcustomtext.keyAction09", "keyAction09.title", "keyAction.09")
+	let keyAction10 = new InsertCustomText(context, "insertcustomtext.keyAction10", "keyAction10.title", "keyAction.10")
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('insertcustomtext.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Insert Custom Text Quickly!');
-	});
-
-	context.subscriptions.push(disposable);
 }
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
